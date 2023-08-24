Miista/Typed

Get rid of DependentValueResolver
Got rid of DependentValueResolver in favor of having DependentValue resolve the value directly.
This was done by injecting the instance into the constructor for DependentValue.

src/Typesafe.With/UnifiedWithBuilder.cs
@@ -20,13 +20,11 @@ namespace Typesafe.With
             if (instance == null) throw new ArgumentNullException(nameof(instance));
             if (properties == null) throw new ArgumentNullException(nameof(properties));
 
-            var valueResolver = new DependentValueResolver<T>(instance);
-            
             // 1. Construct instance of T (and set properties via constructor)
-            var (constructedInstance, remainingPropertiesAfterCtor) = WithByConstructor(instance, properties, _constructorInfo, valueResolver);
+            var (constructedInstance, remainingPropertiesAfterCtor) = WithByConstructor(instance, properties, _constructorInfo);
             
             // 2. Set new properties via property setters
-            var (enrichedInstance, remainingPropertiesAfterPropSet) = EnrichByProperty(constructedInstance, remainingPropertiesAfterCtor, valueResolver);
+            var (enrichedInstance, remainingPropertiesAfterPropSet) = EnrichByProperty(constructedInstance, remainingPropertiesAfterCtor);
 
             if (remainingPropertiesAfterPropSet.Count > 0)
             {
@@ -43,8 +41,7 @@ namespace Typesafe.With
         private static (TInstance Instance, IReadOnlyDictionary<string, object> RemainingProperties) WithByConstructor<TInstance>(
             TInstance instance,
             IReadOnlyDictionary<string, object> newProperties,
-            ConstructorInfo constructorInfo,
-            DependentValueResolver<TInstance> dependentValueResolver)
+            ConstructorInfo constructorInfo)
         {
             var existingProperties = TypeUtils.GetPropertyDictionary<TInstance>();
 
@@ -58,7 +55,7 @@ namespace Typesafe.With
                 var hasNewValue = newProperties.TryGetValue(propertyName, out var newValue);
                 var value = hasNewValue
                     ? newValue is DependentValue dependentValue
-                        ? dependentValueResolver.Resolve(dependentValue, existingProperty)
+                        ? dependentValue.Resolve(existingProperty) //dependentValueResolver.Resolve(dependentValue, existingProperty))
                         : newValue
                     : originalValue;
 
@@ -98,15 +95,13 @@ namespace Typesafe.With
         /// </summary>
         /// <param name="instance">The instance to mutate.</param>
         /// <param name="newProperties">The properties to set.</param>
-        /// <param name="dependentValueResolver">The value resolver.</param>
         /// <typeparam name="TInstance">The instance type.</typeparam>
         /// <returns>A mutated instance.</returns>
         /// <exception cref="InvalidOperationException">If the property does not exist or cannot be written to.</exception>
         /// <exception cref="ArgumentNullException">If any of the arguments are null.</exception>
         private static (TInstance Instance, IReadOnlyDictionary<string, object> RemainingProperties) EnrichByProperty<TInstance>(
             TInstance instance,
-            IReadOnlyDictionary<string, object> newProperties,
-            DependentValueResolver<TInstance> dependentValueResolver)
+            IReadOnlyDictionary<string, object> newProperties)
         {
             var existingProperties = (IDictionary<string, PropertyInfo>) TypeUtils.GetPropertyDictionary<TInstance>();
             var remainingProperties = new Dictionary<string, object>(newProperties.ToDictionary(pair => pair.Key, pair => pair.Value));
@@ -124,7 +119,7 @@ namespace Typesafe.With
                 }
 
                 var value = property.Value is DependentValue dependentValue
-                    ? dependentValueResolver.Resolve(dependentValue, existingProperty)
+                    ? dependentValue.Resolve(existingProperty) //dependentValueResolver.Resolve(dependentValue, existingProperty))
                     : property.Value;
                 
                 existingProperty.SetValue(instance, value);

Miista/Typesafe.With

src/Typesafe.With/UnifiedWithBuilder.cs
@@ -19,13 +19,11 @@ namespace Typesafe.With
             if (instance == null) throw new ArgumentNullException(nameof(instance));
             if (properties == null) throw new ArgumentNullException(nameof(properties));
 
-            var valueResolver = new DependentValueResolver<T>(instance);
-            
             // 1. Construct instance of T (and set properties via constructor)
-            var (constructedInstance, remainingPropertiesAfterCtor) = WithByConstructor(instance, properties, _constructorInfo, valueResolver);
+            var (constructedInstance, remainingPropertiesAfterCtor) = WithByConstructor(instance, properties, _constructorInfo);
             
             // 2. Set new properties via property setters
-            var (enrichedInstance, remainingPropertiesAfterPropSet) = EnrichByProperty(constructedInstance, remainingPropertiesAfterCtor, valueResolver);
+            var (enrichedInstance, remainingPropertiesAfterPropSet) = EnrichByProperty(constructedInstance, remainingPropertiesAfterCtor);
 
             if (remainingPropertiesAfterPropSet.Count > 0)
             {
@@ -42,11 +40,10 @@ namespace Typesafe.With
         private static (TInstance Instance, IDictionary<string, object> RemainingProperties) WithByConstructor<TInstance>(
             TInstance instance,
             IDictionary<string, object> newProperties,
-            ConstructorInfo constructorInfo,
-            DependentValueResolver<TInstance> dependentValueResolver)
+            ConstructorInfo constructorInfo)
         {
             var remainingProperties = new Dictionary<string, object>(newProperties);
-            var parameters = BuildParameters(remainingProperties, constructorInfo, instance, newProperties, dependentValueResolver);
+            var parameters = BuildParameters(remainingProperties, constructorInfo, instance, newProperties);
 
             var constructedInstance = constructorInfo.Invoke(parameters) is TInstance castedInstance
                 ? castedInstance
@@ -59,8 +56,7 @@ namespace Typesafe.With
             IDictionary<string, object> remainingProperties,
             ConstructorInfo constructorInfo,
             TInstance instance,
-            IDictionary<string, object> newProperties,
-            DependentValueResolver<TInstance> dependentValueResolver)
+            IDictionary<string, object> newProperties)
         {
             var existingProperties = TypeUtils.GetPropertyDictionary<TInstance>();
             var resolvedConstructorParameters = new List<object>();
@@ -73,7 +69,7 @@ namespace Typesafe.With
                 var hasNewValue = newProperties.TryGetValue(propertyName, out var newValue);
                 var value = hasNewValue
                     ? newValue is DependentValue dependentValue
-                        ? dependentValueResolver.Resolve(dependentValue, existingProperty)
+                        ? dependentValue.Resolve(existingProperty) //dependentValueResolver.Resolve(dependentValue, existingProperty))
                         : newValue
                     : originalValue;
 
@@ -109,15 +105,13 @@ namespace Typesafe.With
         /// </summary>
         /// <param name="instance">The instance to mutate.</param>
         /// <param name="propertiesToSet">The properties to set.</param>
-        /// <param name="dependentValueResolver">The value resolver.</param>
         /// <typeparam name="TInstance">The instance type.</typeparam>
         /// <returns>A mutated instance.</returns>
         /// <exception cref="InvalidOperationException">If the property does not exist or cannot be written to.</exception>
         /// <exception cref="ArgumentNullException">If any of the arguments are null.</exception>
         private static (TInstance Instance, IDictionary<string, object> RemainingProperties) EnrichByProperty<TInstance>(
             TInstance instance,
-            IDictionary<string, object> propertiesToSet,
-            DependentValueResolver<TInstance> dependentValueResolver)
+            IDictionary<string, object> propertiesToSet)
         {
             var existingProperties = (IDictionary<string, PropertyInfo>) TypeUtils.GetPropertyDictionary<TInstance>();
             var remainingProperties = new Dictionary<string, object>(propertiesToSet);
@@ -135,7 +129,7 @@ namespace Typesafe.With
                 }
 
                 var value = property.Value is DependentValue dependentValue
-                    ? dependentValueResolver.Resolve(dependentValue, existingProperty)
+                    ? dependentValue.Resolve(existingProperty) //dependentValueResolver.Resolve(dependentValue, existingProperty))
                     : property.Value;
                 
                 existingProperty.SetValue(instance, value);