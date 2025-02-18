cbc8292a4fe90c41195bf3b9fb8bb5efab7690b6
Søren Guldmund <palmund@gmail.com>
2025-02-18 10:44:10 +0100

    WIP

---
+++ src/Typesafe.With/ConstructorHelper.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Typesafe.With
+{
+    internal static class ConstructorHelper
+    {
+        public static Dictionary<object, object> ResolveParameterMap(ConstructorInfo constructorInfo)
+        {
+            
+        }
+    }
+}
